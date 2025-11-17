60b1a54642fb6e1ed40b2dcc5453a2de88efd3ad
Jakub Abram <j.abram1@wp.pl>
2025-11-17 18:28:49 +0100

    Created basic register form

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,5 +1,51 @@
-export default function RegisterPage () {
-    return <div>
-        Register Page
-    </div>
+import {FormProvider} from "react-hook-form"
+import { useForm } from "react-hook-form"
+import Input from "../components/Input"
+import Button from "../components/Button"
+
+export default function RegisterPage(){
+    
+    const methods = useForm();
+    const {formState: {errors}} = methods;
+
+    return <FormProvider {...methods}>
+        <form 
+            className="w-full max-w-lg mx-auto px-4"
+        >
+            <h1 className="text-center text-3xl py-6 font-bold">
+                Register Page
+            </h1>
+            {
+                errors.root && <p className="text-xl text-center text-red-600 font-semibold">
+                    {errors.root.message}
+                </p>
+            }
+            <Input 
+                name="name" 
+                id="name" 
+                label="Username"
+                className="mb-4"
+            />
+            <Input 
+                name="email" 
+                id="email" 
+                label="Email Address"
+                type="email"
+                className="mb-4"
+            />
+            <Input 
+                name="password" 
+                id="password" 
+                label="Password"
+                className="mb-4"
+                type="password"
+            />
+            <Button
+                className="w-full"
+                type="submit"
+            >
+                Create Account
+            </Button>
+        </form>
+    </FormProvider>
 }
