96abe1154303b164228a47a8e914e18fb034cc71
Jakub Abram <j.abram1@wp.pl>
2025-11-17 17:45:41 +0100

    Added basic routing for the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import { Navigate, Route, Routes } from "react-router"
+import Navbar from "./Layout/Navbar"
+import UnloggedRoute from "./auth/components/UnloggedRoute"
+import LoginPage from "./pages/LoginPage"
+import RegisterPage from "./pages/RegisterPage"
+import Home from "./pages/Home"
+
 function App() {
-  return <h1 className="text-red-500">
-    Test
-  </h1>
+  return (
+    <Routes>
+      <Route path="/" element={<Navbar />}>
+        <Route index element={<Navigate to="/home" replace={true} />}/>        
+
+        <Route path="home" element={<Home />} />
+
+        <Route path="/auth" element={<UnloggedRoute />}>
+          <Route path="login" element={<LoginPage />}/>
+          <Route path="register" element={<RegisterPage />}/>
+        </Route>
+
+      </Route>
+    </Routes>
+  )
 }
 
 export default App
