561c4f2692b121081c8e7c19e60725b5545860d4
Jakub Abram <j.abram1@wp.pl>
2025-11-17 18:27:11 +0100

    Added basic login page form

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ export default function Button ({children, onClick, disabled, className, type = 
     return <button
         disabled={disabled}
         onClick={onClick}
-        className={`${className} px-10 h-10 flex items-center justify-center gap-4 rounded-lg text-whtie font-semibold border-2 ${!disabled ? "bg-red-600 border-red-800 cursor-pointer hover:bg-red-500 active:bg-red-400" : "bg-red-300 border-red-500 cursor-not-allowed"}`}
+        className={`${className} px-10 h-10 flex items-center justify-center gap-4 rounded-lg text-white font-semibold border-2 ${!disabled ? "bg-red-600 border-red-800 cursor-pointer hover:bg-red-500 active:bg-red-400" : "bg-red-300 border-red-500 cursor-not-allowed"}`}
         type={type}
     >
         {children}
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,3 +1,44 @@
+import {FormProvider} from "react-hook-form"
+import { useForm } from "react-hook-form"
+import Input from "../components/Input"
+import Button from "../components/Button"
+
 export default function LoginPage(){
-    return <div>Login</div>
+    
+    const methods = useForm();
+    const {formState: {errors}} = methods;
+
+    return <FormProvider {...methods}>
+        <form 
+            className="w-full max-w-lg mx-auto px-4"
+        >
+            <h1 className="text-center text-3xl py-6 font-bold">
+                Login Page
+            </h1>
+            {
+                errors.root && <p className="text-xl text-center text-red-600 font-semibold">
+                    {errors.root.message}
+                </p>
+            }
+            <Input 
+                name="name" 
+                id="name" 
+                label="Username"
+                className="mb-4"
+            />
+            <Input 
+                name="password" 
+                id="password" 
+                label="Password"
+                className="mb-4"
+                type="password"
+            />
+            <Button
+                className="w-full"
+                type="submit"
+            >
+                Log in
+            </Button>
+        </form>
+    </FormProvider>
 }
