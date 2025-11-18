62711c15cd523d79b67eca973e18727f7fbc5d48
Jakub Abram <j.abram1@wp.pl>
2025-11-18 14:31:22 +0100

    Created routing for admin dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { Navigate, Route, Routes } from "react-router"
 import Navbar from "./Layout/Navbar"
 import UnloggedRoute from "./auth/components/UnloggedRoute"
+import AdminRoute from "./auth/components/AdminRoute"
 import LoginPage from "./pages/LoginPage"
 import RegisterPage from "./pages/RegisterPage"
 import Home from "./pages/Home"
+import Products from "./pages/admin/Products"
+import NewProductPage from "./pages/admin/NewProductPage"
 
 function App() {
   return (
@@ -17,7 +20,11 @@ function App() {
           <Route path="login" element={<LoginPage />}/>
           <Route path="register" element={<RegisterPage />}/>
         </Route>
-
+      </Route>
+      
+      <Route path="/admin" element={<AdminRoute />}>
+        <Route path="products" element={<Products />}/>
+        <Route path="new-product" element={<NewProductPage />}/>
       </Route>
     </Routes>
   )
