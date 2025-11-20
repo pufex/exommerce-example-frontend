c1b3a096dc1f28ca13da67d6dd70125d2f0b8136
Jakub Abram <j.abram1@wp.pl>
2025-11-20 20:25:33 +0100

    Added some responsive design to the admin products dashboard page

--- src/components/admin/DeleteProductBtn.jsx
+++ src/components/admin/DeleteProductBtn.jsx
@@ -1,37 +1,51 @@
 import Button from "../Button";
 import { useAxiosPrivate } from "../../auth/hooks/useAxiosPrivate";
 import { useState } from "react";
-import { LoaderCircle } from "lucide-react";
+import { LoaderCircle, Trash2 } from "lucide-react";
+import { useWindowSize } from "../../hooks/useWindowSizes"
+import IconButton from "../IconButton"
 
-export default function DeleteProductBtn({product_id, setProducts}){
+export default function DeleteProductBtn({ product_id, setProducts }) {
 
     const [loading, setLoading] = useState(false)
+    const { width: windowWidth } = useWindowSize()
 
     const axiosPrivate = useAxiosPrivate();
 
     const handleClick = async () => {
-        try{
+        try {
             setLoading(true)
             await axiosPrivate.delete(`/admin/delete-product/${product_id}`)
             setProducts(products => products.filter((product) => product._id != product_id))
-        }catch(err){
+        } catch (err) {
             console.log(err)
-        }finally{
+        } finally {
             setLoading(false)
         }
     }
-    
-    return <Button
-        onClick={handleClick}
-        disabled={loading}
-    >
-        {
-            !loading
-                ? "Delete"
-                : <>
-                    Deleting...
-                    <LoaderCircle className="w-6 h-6 text-white animate-spin"/>
-                </>
-        }
-    </Button>
+
+    return windowWidth > 600
+        ? <Button
+            onClick={handleClick}
+            disabled={loading}
+        >
+            {
+                !loading
+                    ? "Delete"
+                    : <>
+                        Deleting...
+                        <LoaderCircle className="w-6 h-6 text-white animate-spin" />
+                    </>
+            }
+        </Button>
+        : <IconButton
+            onClick={handleClick}
+            disabled={loading}
+        >
+            {
+                !loading
+                    ? <Trash2 className="w-6 h-6" />
+                    : <LoaderCircle className="w-6 h-6 text-white animate-spin"/>
+            }
+        </IconButton>
 }
